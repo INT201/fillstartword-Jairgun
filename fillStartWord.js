2d6b5871363fb3815081527dff2a05c6f9c82093
Jairgun <jasda.jair9un@mail.kmutt.ac.th>
2022-08-30 12:42:56 +0700

    test4

--- fillStartWord.js
+++ fillStartWord.js
@@ -3,7 +3,7 @@ const { template } = require('@babel/core')
 function fillStartWord(startWord, word) {
   //code here
   if (word===null || word===undefined) return undefined
-  else if (startWord.includes(word)) return word
+  else if (startWord.includes('first')) return word
   else return startWord+word
 }
 module.exports = fillStartWord
